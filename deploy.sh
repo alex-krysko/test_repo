8035c8641c109b3bbf8cd92e780900c5b092c57b
alex-krysko <65822124+alex-krysko@users.noreply.github.com>
2020-08-11 12:59:45 +0300

    Add files via upload

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-
+aws ec2 create-default-vpc
+aws ec2 create-default-subnet --availability-zone us-east-2a
 ansible-playbook -i ./hosts.txt ./create_playbook.yml && ansible-playbook -i ./hosts.txt ./playbook.yml
