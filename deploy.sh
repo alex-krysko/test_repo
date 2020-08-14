1d71136ee662e8989049d93e64f977b53872c699
alex-krysko <65822124+alex-krysko@users.noreply.github.com>
2020-08-14 03:23:55 +0300

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-aws ec2 create-default-vpc
-aws ec2 create-default-subnet --availability-zone us-east-2a
-ansible-playbook -i ./hosts.txt ./create_playbook.yml && ansible-playbook -i ./hosts.txt ./playbook.yml
